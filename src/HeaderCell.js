cb7a6720f3da0f76cbfb91ba0fd35f40e7057583
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-01-09 19:40:16 +0100

    Remove checkwidth (old implementation)

--- src/HeaderCell.js
+++ src/HeaderCell.js
@@ -1,76 +1,58 @@
 import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 class HeaderCell extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.clearTimeoutIdForSizesUpdater = null;
+  constructor (props) {
+    super(props)
+    this.clearTimeoutIdForSizesUpdater = null
   }
 
-  componentDidMount() {
-    this.checkWidth();
+  componentDidMount () {
+    this.checkWidth()
   }
 
-  componentDidUpdate(prevProps) {
-    this.checkWidth();
+  componentDidUpdate (prevProps) {
+    this.checkWidth()
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.timeout);
+  componentWillUnmount () {
+    clearTimeout(this.timeout)
 
     if (this.clearTimeoutIdForSizesUpdater) {
-      clearTimeout(this.clearTimeoutIdForSizesUpdater);
+      clearTimeout(this.clearTimeoutIdForSizesUpdater)
     }
   }
 
-  checkWidth() {
-    const { onWidthChange } = this.props;
-
-    if (onWidthChange && this.clearTimeoutIdForSizesUpdater === null) {
-      this.clearTimeoutIdForSizesUpdater = setTimeout(() => {
-        this.clearTimeoutIdForSizesUpdater = null;
-
-        const { width, row, col } = this.props;
-        const bcr = this.cellDomNode.getBoundingClientRect();
-
-        if (width != bcr.width + 'px') {
-          onWidthChange(row, col, bcr.width);
-        }
-      }, 5);
-    }
-  }
-  render() {
+  render () {
     const {
       row, col, rowSpan, colSpan, width,
       overflow, className, value, component,
       attributes, fixed, left
-    } = this.props;
-    const style = { width, left };
+    } = this.props
+    const style = { width, left }
     const fullCN = [
       className, 'header-cell', 'cell',
       'read-only', overflow, fixed && 'fixed-column'
-    ].filter(a => a).join(' ');
+    ].filter(a => a).join(' ')
 
     return (
       <th
-        ref={ ref => this.cellDomNode = ref }
-        className={ fullCN }
-        colSpan={ colSpan || 1 }
-        rowSpan={ rowSpan || 1 }
-        style={ style }
-        { ...attributes }
+        ref={ref => this.cellDomNode = ref}
+        className={fullCN}
+        colSpan={colSpan || 1}
+        rowSpan={rowSpan || 1}
+        style={style}
+        {...attributes}
       >
         {
-          component ?
-            component :
-            (
-              <span style={{display: 'block'}}>
-                { value }
-              </span>
+          component || (
+          <span style={{display: 'block'}}>
+            { value }
+          </span>
             )
         }
       </th>
-    );
+    )
   }
 }
 
@@ -85,8 +67,7 @@ HeaderCell.propTypes = {
   component: PropTypes.element,
   attributes: PropTypes.object,
   fixed: PropTypes.bool,
-  left: PropTypes.string,
-  onWidthChange: PropTypes.func
-};
+  left: PropTypes.string
+}
 
-export default HeaderCell;
+export default HeaderCell
