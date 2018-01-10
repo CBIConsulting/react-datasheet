5128d7c5865d95acb9d3342277ede3a277060849
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-01-10 21:09:28 +0100

    Add test cases for common functions (utils)

--- test/common.js
+++ test/common.js
@@ -6,82 +6,129 @@ import {
   handlePasteLogic
 } from '../src/utils/dataSheet'
 
-describe('Utility functions', () => {
-  const copyData = '5\t2\n1\t6';
-  const eventSimulation = {
-    preventDefault: () => {},
-    clipboardData: {
-      getData(type) {
-        return copyData
-      }
-    }
-  };
-  const data = [
-    [{value: 5}, {value: 2}],
-    [{value: 1}, {value: 6}],
-  ];
-  const valueRenderer = cell => cell.value;
-  const props = {data, valueRenderer};
-  const state = {
-    start: {i: 0, j: 0},
-    end: {i: 1, j: 1}
-  };
+import {
+  isEmptyObj,
+  nullFunction,
+  cellStateComparison,
+  isCellSelected,
+  isUndefined,
+  range
+} from '../src/utils/utils'
 
-  it('handleCopyLogic', () => {
-    expect(handleCopyLogic(eventSimulation, props, state)).toEqual(copyData)
-  })
-
-  it('handlePasteLogic', () => {
-    expect(handlePasteLogic(eventSimulation, props, state)).toEqual({
-      changedCells: [
-        {
-          cell: {value: 5},
-          i: 0,
-          j: 0,
-          value: '5'
-        },
-        {
-          cell: {value: 2},
-          i: 0,
-          j: 1,
-          value: '2'
-        },
-        {
-          cell: {value: 1},
-          i: 1,
-          j: 0,
-          value: '1'
-        },
-        {
-          cell: {value: 6},
-          i: 1,
-          j: 1,
-          value: '6'
+describe('Utils', () => {
+  describe('Logic functions', () => {
+    const copyData = '5\t2\n1\t6';
+    const eventSimulation = {
+      preventDefault: () => {},
+      clipboardData: {
+        getData(type) {
+          return copyData
         }
-      ],
-      end: {i: 1, j: 1},
-      pastedData: [
-        [
+      }
+    };
+    const data = [
+      [{value: 5}, {value: 2}],
+      [{value: 1}, {value: 6}],
+    ];
+    const valueRenderer = cell => cell.value;
+    const props = {data, valueRenderer};
+    const state = {
+      start: {i: 0, j: 0},
+      end: {i: 1, j: 1}
+    };
+
+    it('handleCopyLogic', () => {
+      expect(handleCopyLogic(eventSimulation, props, state)).toEqual(copyData)
+    })
+
+    it('handlePasteLogic', () => {
+      expect(handlePasteLogic(eventSimulation, props, state)).toEqual({
+        changedCells: [
           {
             cell: {value: 5},
-            data: '5'
+            i: 0,
+            j: 0,
+            value: '5'
           },
           {
             cell: {value: 2},
-            data: '2'
-          }
-        ],
-        [
+            i: 0,
+            j: 1,
+            value: '2'
+          },
           {
             cell: {value: 1},
-            data: '1'
+            i: 1,
+            j: 0,
+            value: '1'
           },
           {
             cell: {value: 6},
-            data: '6'
+            i: 1,
+            j: 1,
+            value: '6'
           }
+        ],
+        end: {i: 1, j: 1},
+        pastedData: [
+          [
+            {
+              cell: {value: 5},
+              data: '5'
+            },
+            {
+              cell: {value: 2},
+              data: '2'
+            }
+          ],
+          [
+            {
+              cell: {value: 1},
+              data: '1'
+            },
+            {
+              cell: {value: 6},
+              data: '6'
+            }
+          ]
         ]
-      ]
+      })
+    })
+
+    it('handleKeyLogic', () => {
+      expect(true).toBe(true);
+    })
+  })
+
+  describe('Utility functions', () => {
+    it('isEmptyObj', () => {
+      expect(isEmptyObj({})).toBe(true);
+      expect(isEmptyObj({a:1})).toBe(false);
+    })
+
+    it('nullFunction', () => {
+      expect(nullFunction()).toEqual(undefined);
+    })
+
+    it('cellStateComparison', () => {
+      expect(cellStateComparison({i: 1, j: 5}, 1, 5)).toBe(true);
+      expect(cellStateComparison({i: 1, j: 5}, 2, 5)).toBe(false);
+    })
+
+    it('isCellSelected', () => {
+      expect(true).toBe(true);
+    })
+
+    it('isUndefined', () => {
+      expect(isUndefined(undefined)).toBe(true);
+      expect(isUndefined(null)).toBe(false);
+      expect(isUndefined(0)).toBe(false);
+      expect(isUndefined('undefined')).toBe(false);
+      expect(isUndefined(true)).toBe(false);
+    })
+
+    it('range', () => {
+      expect(true).toBe(true);
     })
   })
 })
